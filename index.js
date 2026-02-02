1bbf0d1d967ac1b35c57eaffee5268964e575e12
Dishachauhan05 <dishac787@gmail.com>
2026-02-02 17:24:41 +0530

    first commit

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ app.use("/todos", todoRoutes);
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
+
--- routes/todos.js
+++ routes/todos.js
@@ -18,4 +18,59 @@ router.post("/", async (req, res) => {
   }
 });
 
+
+router.put("/:id",async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const{description,completed} = req.body;
+
+        const updatedTodo = await pool.query(
+            "UPDATE todo SET description = $1,completed =$2 WHERE todo_id = $3 RETURNING *",
+            [description,completed,id] 
+        );
+        res.json({
+            message:"Todo was updated!",
+            todo : updatedTodo.rows[0],
+        });
+    }
+    catch(err){ 
+    console.log(err.message);
+    res.status(500).send("Server Error");
+    }
+})
+
+
+router.get("/list",async(req,res)=>{
+    try{
+    
+     const getTodo = await pool.query(
+        "SELECT * FROM todo",
+    );
+     res.json(getTodo.rows);
+    }
+    catch(err){
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+
+
+router.delete("/:id",async(req,res)=>{
+    try{
+    const {id} = req.params;
+    
+    const deleteTodo =await pool.query(
+        "DELETE FROM todo WHERE todo_id =$1 RETURNING *",
+        [id]
+    );
+    res.json(deleteTodo.rows[0]);
+}
+    catch(err){  
+      console.log(err.message);
+      res.status(500).send("Server Error");
+  }
+})
+
 module.exports = router;
